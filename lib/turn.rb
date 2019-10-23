48cdb836ce4bd3631a843da7cc97c9886a76882c
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-23 00:48:43 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,10 @@
+def display_board(
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts 'display board'
+#board output
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
